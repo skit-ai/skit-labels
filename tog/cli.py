f81b1e8b4815fa3d10ee2d60835c9a5c22cb8fe0
ltbringer <amresh.venugopal@gmail.com>
2022-01-09 01:32:16 +0530

    refactor: argparse replaces docopt.

--- tog/cli.py
+++ tog/cli.py
@@ -1,98 +1,124 @@
 """
 Command line interface for interacting with a tog data server.
-
-Usage:
-  tog download --job-id=<job-id> --output-sqlite=<output-sqlite>
-    [--batch-size=<batch-size>] [--all] [--task-type=<task-type>]
-  tog describe --job-id=<job-id>
-  tog stats --job-id=<job-id>
-  tog list
-  tog --version
-  tog (-h|--help)
-
-Options:
-  --job-id=<job-id>                 Id of the tog job that we want to download
-  --output-sqlite=<output-sqlite>   Output sqlite file path
-  --batch-size=<batch-size>         Number of items to download in a batch via server sided cursor [default: 500]
-  --all                             If provided, download all data instead of only tagged ones.
-  --task-type=<task-type>           Task type for deserialization [default: dict]
 """
-
 import os
+import pytz
+import argparse
 
-import attr
-from docopt import docopt
-from tqdm import tqdm
-
-from tog import __version__
-from tog.db import Database, Job, SqliteDatabase
+import toml
+from tog import constants as const
+from tog import commands
 
 
-def batch_gen(source, n=100):
-    """
-    Batched generator
-    """
+def is_timezone(value):
+    if value not in pytz.all_timezones:
+        raise argparse.ArgumentTypeError(
+            f"Unknown timezone {value}. Lookup `pytz.all_timezones`."
+        )
 
-    batch = []
 
-    for it in source:
-        batch.append(it)
-        if len(batch) % n == 0:
-            yield batch
-            batch = []
+def build_download_command(parser: argparse.ArgumentParser) -> None:
+    parser.add_argument(
+        "-j",
+        "--job-id",
+        type=int,
+        help="Id of the tog dataset that we want to download.",
+    )
+    parser.add_argument(
+        "-o",
+        "--output-format",
+        type=str,
+        help="Store dataset in supported formats.",
+        choices=[".csv", ".sqlite"],
+    )
+    parser.add_argument(
+        "--timezone",
+        type=is_timezone,
+        help="Timezone to parse datetime values. Like 'America/Los_Angeles', 'Asia/Kolkata' etc.",
+        default=pytz.UTC,
+    )
+    parser.add_argument(
+        "--batch-size",
+        type=int,
+        default=500,
+        help="Number of items to download in a batch.",
+    )
+    parser.add_argument(
+        "--full",
+        action="store_true",
+        help="If provided, download all data instead of including untagged datapoints.",
+    )
+    parser.add_argument(
+        "--task-type",
+        type=str,
+        default=const.TASK_TYPE__DICT,
+        help="Task type for deserialization.",
+        choices=const.TASK_TYPES,
+    )
 
-    if batch:
-        yield batch
+
+def build_describe_command(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        "--job-id", type=int, help="Id of the tog dataset that we want to describe."
+    )
+
+
+def build_stats_command(parser: argparse.ArgumentParser):
+    parser.add_argument(
+        "--job-id",
+        type=int,
+        help="Check the state of the dataset i.e tagged, "
+        "untagged and pending data points for a given job-id.",
+    )
+
+
+def build_parser():
+    with open("pyproject.toml") as handle:
+        project_metadata = toml.load(handle)
+
+    version = project_metadata["tool"]["poetry"]["version"]
+    parser = argparse.ArgumentParser(
+        description=f"tog-cli {version}. Command line interface for interacting with data server.",
+    )
+    command_parsers = parser.add_subparsers(dest="command")
+    build_download_command(
+        command_parsers.add_parser(
+            const.DOWNLOAD,
+            help="Download a dataset for a given tog id.",
+            formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+        )
+    )
+    build_describe_command(
+        command_parsers.add_parser(
+            const.DESCRIBE, help="Describe a dataset for a given tog id."
+        )
+    )
+    build_stats_command(
+        command_parsers.add_parser(
+            const.STATS, help="Get tagged/untagged points for a given tog id."
+        )
+    )
+    return parser
 
 
 def main():
-    args = docopt(__doc__, version=__version__)
-
-    if args["download"]:
-        job = Job(int(args["--job-id"]), task_type=args["--task-type"])
-
-        output_filepath = args["--output-sqlite"]
-        if os.path.exists(output_filepath):
-            raise RuntimeError(f"File already exists {output_filepath}")
-
-        sdb = SqliteDatabase(output_filepath)
-        bar = tqdm(total=job.total(untagged=args["--all"]))
-
-        print(f"Downloading job {job.id}: {job.name} [language: {job.lang}]\n{job.description}")
-
-        for items in batch_gen(job.get(untagged=args["--all"]), n=int(args["--batch-size"])):
-            rows = []
-            for task, tag, tagged_time in items:
-                # For raw dictionary type tasks, we don't use attr classes.
-                if isinstance(task, dict):
-                    task_dict = task
-                else:
-                    task_dict = attr.asdict(task)
-
-                # TODO: is_gold might not be working for dict type tasks as of now
-                rows.append((task.id, task_dict, tag, task.is_gold, tagged_time))
-
-            sdb.insert_rows(rows)
-            bar.update(n=len(items))
-
-    elif args["describe"]:
-        job = Job(int(args["--job-id"]))
-
-        print(f"Job {job.id}: {job.name} [language: {job.lang}]\n{job.description}")
-
-    elif args["stats"]:
-        job = Job(int(args["--job-id"]))
-
-        n_total = job.total(untagged=True)
-        n_tagged = job.total()
-
-        print(f"Total items {n_total}. Tagged {n_tagged}. Untagged {n_total - n_tagged}.")
-
-    elif args["list"]:
-        db = Database()
-
-        jobs = db.list_jobs()
-        print(f"Total {len(jobs)} active jobs found\n")
-
-        for i, job in enumerate(jobs):
-            print(f"{i + 1}. Job {job['id']}: {job['name']} [language: {job['language']}]")
+    parser = build_parser()
+    args = parser.parse_args()
+    if args.command == const.DOWNLOAD:
+        sdb, sdb_path = commands.download_dataset(
+            args.job_id,
+            args.task_type,
+            args.timezone,
+            args.full,
+            args.batch_size,
+        )
+        if args.output_format == const.OUTPUT_FORMAT__CSV:
+            df_path = commands.sdb2df(sdb, args.job_id)
+            print(f"Saved dataframe to {df_path}.")
+            os.remove(sdb_path)
+        else:
+            print(f"Saved sqlite database to {sdb_path}.")
+    elif args.command == const.DESCRIBE:
+        commands.describe_dataset(args.job_id)
+    elif args.command == const.STATS:
+        commands.stat_dataset(args.job_id)
